ef9fa5f588f39b6461c7b93ae1d81f34a5bf9547
wulman16 <will.ulman@gmail.com>
2019-04-05 16:28:53 -0400

    done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,29 @@
-// create your App component here
+import React, { Component } from 'react'
+import 'isomorphic-fetch';
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+class App extends Component {
+
+  state = {
+    peopleInSpace: []
+  }
+
+  componentDidMount() {
+    fetch(`http://api.open-notify.org/astros.json`)
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        peopleInSpace: data.people
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>{this.state.peopleInSpace.map(person => person.name)}</div>
+    )
+  }
+}
+
+export default App
